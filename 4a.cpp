01fef2f5c6e5aa102a6042e95023bb21d0f691a3
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-12 16:44:03 -0600

    created 2a file and added code in 4a

---
+++ 4a.cpp
@@ -0,0 +1,16 @@
+void 3a_Intersection(L) {
+	
+	for ( position element1 = Primera(L); element1 != NULL; element1 = Siguiente(element1, L) ) {
+		for ( position element2 = Primera(L); element2 != NULL; element2 = Siguiente(element2, L) ) {
+			if ( 
+		
+		}
+	}
+	// while ( elementL1 != NULL ) {
+		//  while ( elementL2 != NULL ) {
+			
+		// 	elementL2 = Siguiente(elementL2, L2)
+		// }
+		//elementL1 = Siguiente(elementL1, L1)
+	//}
+}
