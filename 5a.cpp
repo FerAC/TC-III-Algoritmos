01b743a37b69fb14dad2f56b26b8fdc7702b365e
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2022-09-12 17:49:03 -0600

    Micro cambios

---
+++ 5a.cpp
@@ -0,0 +1,8 @@
+/* ENUNCIADO
+ * Insertar en una lista ordenada un nuevo elemento de tal manera que la lista resultado
+ * quede ordenada.
+ * 
+ * Lista Indexada
+ * Ejemplo: Si L = 1, 7, 8, 9, 9, 10, 11 y E = 6
+ * Entonces L deberá ser igual a 1, 6, 7, 8, 9, 9, 10, 11
+*/
