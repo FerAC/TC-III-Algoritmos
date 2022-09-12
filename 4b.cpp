249018fc89da976e8d95a01f8e7f2a95ecc38693
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-12 15:04:53 -0600

    Create 4b.cpp

---
+++ 4b.cpp
@@ -0,0 +1,42 @@
+// function return a list
+list simetricDifference(L1, L2) {
+	index elementL1 = 0	 // index is an unsigned int
+	index elementL2 = 0
+	index elementL3 = 0
+	bool condition = true
+	Iniciar(L3);
+	
+	while ( elementL1 <= NumElemt(L1) ) {
+		while ( (condition == true) && ( elementL2 <= NumElemt(L2) ) ) {
+			if ( Recuperar(elementL1, L1) !=  Recuperar(elementL2++, L2) ) {
+				condition = false
+			}
+		}
+		if (condition == false) {
+			Insertar(elementL3, Recuperar(elementL1, L1), L3)
+			++elementL3
+			condition = true
+		}
+		++elementL1
+		elementL2 = 0
+	}
+	
+	elementL1 = 0
+	
+	while ( elementL2 <= NumElemt(L2) ) {
+		while ( (condition == true) && ( elementL1 <= NumElemt(L1) ) ) {
+			if ( Recuperar(elementL2, L2) !=  Recuperar(elementL1++, L1) ) {
+				condition = false
+			}
+		}
+		if (condition == false) {
+			Insertar(elementL3, Recuperar(elementL2, L2), L3)
+			++elementL3
+			condition = true
+		}
+		++elementL2
+		elementL1 = 0
+	}
+	
+	return L3
+}
