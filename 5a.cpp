567a6fa3239e707441a6a7b8370a4b54a0bb0add
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2022-09-12 18:37:09 -0600

    Problema 5a terminado

--- 5a.cpp
+++ 5a.cpp
@@ -1,8 +1,37 @@
 /* ENUNCIADO
- * Insertar en una lista ordenada un nuevo elemento de tal manera que la lista resultado
- * quede ordenada.
+ * Eliminar los elementos repetidos de una lista.
  * 
  * Lista Indexada
- * Ejemplo: Si L = 1, 7, 8, 9, 9, 10, 11 y E = 6
- * Entonces L deberá ser igual a 1, 6, 7, 8, 9, 9, 10, 11
-*/
+ * Ejemplo: si L = 7, 1, 7, 9, 1, 7, 3
+ * Entonces L deberá ser igual a 7, 1, 9, 3
+ */
+
+void Remove_Duplicates(Lista i, Elemento e)
+{
+    // Get the list size for list end
+    int size = NumElem(i);
+
+    // Iterative linear, nested search
+    // Space complexity of O(1) yet time complexity of O(n^2)
+    for(int iter_a = 0, iter_a < size, iter_a = iter_a + 1)
+    {
+        // Element to find matches for
+        Elemento a = Recuperar(iter_a, i)
+
+        // Linear search to find duplicates
+        // Problem has been reduced to a compariso of n-k elements
+        for(int iter_b = a + 1, iter_b < size, iter_b = iter_a + 1)
+        {
+            Elemento b = Recuperar(iter_b, i)
+
+            if(a == b)
+            {
+                Borrar(iter_b, i)
+                iter_b = iter_b - 1
+            }
+        }
+    }
+
+    // We're done :)
+    return
+}
--- 5b.cpp
+++ 5b.cpp
@@ -5,9 +5,9 @@
  * Lista Indexada
  * Ejemplo: Si L = 1, 7, 8, 9, 9, 10, 11 y E = 6
  * Entonces L deberá ser igual a 1, 6, 7, 8, 9, 9, 10, 11
-*/
+ */
 
-void Insercion_Ordenada(Lista i, Elemento e)
+void Sorted_Insertion(Lista i, Elemento e)
 {
     // Get the list size for edge partitioning
     int size = NumElem(i);
