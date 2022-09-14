3a826f5571affeef72de33ebd7b4ddcb4d588917
FerAC <87688596+FerAC@users.noreply.github.com>
2022-09-14 15:33:10 -0600

    No repite elementos

--- 1b.cpp
+++ 1b.cpp
@@ -8,22 +8,30 @@ Lista union(L1, L2){
 	int sizeL2 = numElem(L2)
 	int cntr = 0
 	
-	while( cntr < sizeL3) {
+	while( p1 != nullpos && p2 != nullpos) {
 		if(sizeL1==0){
-			AgregarAlFinal(Recuperar( p2, L1) , L3)
+			AgregarAlFinal(Recuperar( p2, L2) , L3)
+			p2 = Siguiente(p2, L2);
 		} else if( sizeL2== 0){
 			AgregarAlFinal(Recuperar( p1, L1) , L3)
+			p1 = Siguiente(p1, L1);
 		} 
 		if( sizeL1!=0 && sizeL2!=0){
-			if( Recuperar( p1, L1)  <= Recuperar( p2, L2)  ){
+			if( Recuperar( p1, L1)  < Recuperar( p2, L2)  ){
 				AgregarAlFinal(Recuperar( p1, L1) , L3)
 				--sizeL1;
 				p1 = Siguiente(p1, L1)
 				
-			} else{
-				AgregarAlFinal(Recuperar( p2, L1) , L3)
+			} else if Recuperar( p1, L1)  > Recuperar( p2, L2) {
+				AgregarAlFinal(Recuperar( p2, L2) , L3) 
 				p2 = Siguiente(p2, L2)
 				--sizeL2 
+			} else{
+				AgregarAlFinal(Recuperar( p1, L1) , L3)
+				p1 = Siguiente(p1, L1)
+				p2 = Siguiente(p2, L2)
+				--sizeL1
+				--sizeL2
 			}
 			++cntr
 		}
