0978399e50944bdeddf3522d16f69d6fc2a5ae5f
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-14 07:41:49 -0600

    changed NULL for nullPos

--- 3a.cpp
+++ 3a.cpp
@@ -4,7 +4,7 @@ list 3a_mergeUnion(L1, L2) {
 	position element = Primera(L1)
 	bool exist = true
 	
-	while ( element != NULL ) {
+	while ( element != nullPos ) {
 		AgregarAlFinal(Recuperar(element, L1), L3)
 		element = Siguiente(element, L1)
 		// elementL3 = Siguiente(elementL3, L3)
@@ -12,10 +12,10 @@ list 3a_mergeUnion(L1, L2) {
 	
 	element = Primera(L2)
 	
-	while ( element != NULL ) {
+	while ( element != nullPos ) {
 		position elementL3 = Primera(L3) 
 		// loop to verify if the element already exist in the L3 list
-		while ( elementL3 != NULL && existe==true) {
+		while ( elementL3 != nullPos && existe==true) {
 			if ( Recuperar(element, L2) != Recuperar(elementL3, L3) ) {
 				exist = false
 			}
