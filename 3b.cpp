7723a26d5b49d711ba43860a7d0bf5952275751f
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2022-09-14 17:03:40 -0600

    3B terminado

--- 3b.cpp
+++ 3b.cpp
@@ -3,19 +3,23 @@
  * Crear una lista L3, no ordenada, que contenga los elementos que pertenecen a C1 ∩ C2
  */
 
-// function return a list
-list 3a_Intersection(L1, L2) {
+// Function return a list
+List 3B_Intersection(L1, L2) 
+{
 	Iniciar(L3)
-	position elementL1 = Primera(L1)
-	position elementL2 = Primera(L2)
+	Position elementL1 = Primera(L1)
+	Position elementL2 = Primera(L2)
 	
-	while ( elementL1 != nullPos ) {
-		while ( elementL2 != nullPos ) {
-			if ( Recuperar(elementL1, L1) == Recuperar(elementL2, L2) {
+	while (elementL1 != nullPos) 
+	{
+		while (elementL2 != nullPos) 
+		{
+			if (Recuperar(elementL1, L1) == Recuperar(elementL2, L2))
 				AgregarAlFinal(Recuperar(element, L1), L3)
-			}
+			
 			elementL2 = Siguiente(elementL2, L2)
 		}
+
 		elementL1 = Siguiente(elementL1, L1)
 	}
 	return L3
