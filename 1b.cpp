9afcc4b322b6498b078f76a2418ecabfc69fd232
FerAC <87688596+FerAC@users.noreply.github.com>
2022-09-13 22:09:49 -0600

    Variables ingles

--- 1b.cpp
+++ 1b.cpp
@@ -1,31 +1,31 @@
 Lista union(L1, L2){
-	posicion p1 = Primera(L1);
-	posicion p2 = Primera(L2);
-	Lista L3;
-	Iniciar(L3);
-	int tamanoL3 = numElem(L1) + numElem(L2);
-	int tamanoL1 = numElem(L1);
-	int tamanoL2 = numElem(L2);
-	int contador = 0;
+	position p1 = Primera(L1)
+	position p2 = Primera(L2)
+	List L3
+	Iniciar(L3)
+	int sizeL3 = numElem(L1) + numElem(L2)
+	int sizeL1 = numElem(L1)
+	int sizeL2 = numElem(L2)
+	int cntr = 0
 	
-	while( contador < tamanoL3) {
-		if(tamanoL1==0){
-			AgregarAlFinal(Recuperar( p2, L1) , L3);
-		} else if( tamanoL2== 0){
-			AgregarAlFinal(Recuperar( p1, L1) , L3);
+	while( cntr < sizeL3) {
+		if(sizeL1==0){
+			AgregarAlFinal(Recuperar( p2, L1) , L3)
+		} else if( sizeL2== 0){
+			AgregarAlFinal(Recuperar( p1, L1) , L3)
 		} 
-		if( tamanoL1!=0 && tamanoL2!=0){
+		if( sizeL1!=0 && sizeL2!=0){
 			if( Recuperar( p1, L1)  <= Recuperar( p2, L2)  ){
-				AgregarAlFinal(Recuperar( p1, L1) , L3);
-				--tamanoL1;
-				p1 = Siguiente(p1, L1);
+				AgregarAlFinal(Recuperar( p1, L1) , L3)
+				--sizeL1;
+				p1 = Siguiente(p1, L1)
 				
 			} else{
-				AgregarAlFinal(Recuperar( p2, L1) , L3);
-				p2 = Siguiente(p2, L2);
-				--tamanoL2; 
+				AgregarAlFinal(Recuperar( p2, L1) , L3)
+				p2 = Siguiente(p2, L2)
+				--sizeL2 
 			}
-			++contador; 
+			++cntr
 		}
 		
 }
