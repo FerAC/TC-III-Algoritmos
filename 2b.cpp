2d42c4c7b267956d447fcec85b461d507b2620a2
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-14 11:35:08 -0600

    deleted a few useless comment

--- 2b.cpp
+++ 2b.cpp
@@ -9,9 +9,7 @@ list simetricDifference(L1, L2) {
 	index elementL1 = 0		// index is an unsigned int
 	index elementL2 = 0
 	index elementL3 = 0
-	//elementL1 -> 4
-	//L1 -> 
-	//L2 -> 10, 11, 12, 13, 14, 15
+
 	while ( elementL1<sizeL1 and elementL2<sizeL2) {
 		if(Recuperar(elementL1, L1) < Recuperar(elementL2, L2) ){
 			Insertar(elementL3, Recuperar(elementL1, L1), L3)
