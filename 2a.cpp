4420f8dbe6487c81c07d4fc9a22677ff07e3c9a8
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-14 11:34:16 -0600

    made a few changes

--- 2a.cpp
+++ 2a.cpp
@@ -1,5 +1,5 @@
 // function return a list
-list simetricDifference(L1, L2) {
+list 2a_C1minusC2(L1, L2) {
 	bool condition = true
 	Iniciar(L3)
 	
@@ -9,9 +9,7 @@ list simetricDifference(L1, L2) {
 	index elementL1 = 0		// index is an unsigned int
 	index elementL2 = 0
 	index elementL3 = 0
-	//elementL1 -> 4
-	//L1 -> 
-	//L2 -> 10, 11, 12, 13, 14, 15
+
 	while ( elementL1<sizeL1 and elementL2<sizeL2) {
 		if(Recuperar(elementL1, L1) < Recuperar(elementL2, L2) ){
 			Insertar(elementL3, Recuperar(elementL1, L1), L3)
@@ -19,7 +17,6 @@ list simetricDifference(L1, L2) {
 			++elementL3
 		}
 		elif (Recuperar(elementL2, L2) < Recuperar(elementL1, L1) ) {
-			//Insertar(elementL3, Recuperar(elementL2, L2), L3)
 			++elementL2
 			++elementL3
 		}
@@ -34,12 +31,6 @@ list simetricDifference(L1, L2) {
 		++elementL1
 		++elementL3
     }
-	/*
-    while (elementL2<sizeL2) {
-        Insertar(elementL3, Recuperar(elementL2, L2), L3)
-		++elementL2
-		++elementL3
-    }	*/
-	
+
 	return L3
 }
