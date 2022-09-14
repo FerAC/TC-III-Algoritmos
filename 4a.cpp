edf1af05ba78266bad5c7bbeec76a98bf3b1992c
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2022-09-14 16:58:46 -0600

    4A resuelto

--- 4a.cpp
+++ 4a.cpp
@@ -3,21 +3,26 @@
  * Ordenar una lista usando el algoritmo Burbuja
  */
 
-void 3a_Intersection(L)
+void 4A_Bubble_Sort(L)
 {
-	
-	for (position element1 = Primera(L); element1 != NULL; element1 = Siguiente(element1, L) ) 
+	bool must_continue = true
+
+	while(must_continue == true) 
 	{
-		for ( position element2 = Primera(L); element2 != NULL; element2 = Siguiente(element2, L) ) 
+		must_continue = false
+
+		for (Position element2 = Primera(L), element2 != NULL, element2 = Siguiente(element2, L)) 
 		{
-			if ( Recuperar(element2, L) > Recuperar(Siguiente(element2, L)) 
+			if (Recuperar(element2, L) > Recuperar(Siguiente(element2, L)) 
 			{
-				element temporal = Recuperar(element2, L)
-				Modificar( element2, Recuperar(element2+1, L), L)
-				Modificar( element2+1, temporal, L)				
-			}		
+				Element temporal = Recuperar(element2, L)
+				Modificar(element2, Recuperar(Siguiente(element2, L), L)
+				Modificar(Siguiente(element2, L), temporal, L)				
+
+				must_continue = true
+			}
 		}
 	}
-	
-	return L
+
+	return
 }
