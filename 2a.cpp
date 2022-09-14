76a787a35e1a2bdbc0e6bf619eedb93f88aa7bf7
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-14 11:29:37 -0600

    Update 2a.cpp

--- 2a.cpp
+++ 2a.cpp
@@ -1,25 +1,45 @@
 // function return a list
 list simetricDifference(L1, L2) {
-	index elementL1 = 0	 // index is an unsigned int
-	index elementL2 = 0
-	index elementL3 = 0
 	bool condition = true
 	Iniciar(L3)
 	
-	while ( elementL1 <= NumElemt(L1) ) {
-		while ( (condition == true) && ( elementL2 <= NumElemt(L2) ) ) {
-			if ( Recuperar(elementL1, L1) !=  Recuperar(elementL2++, L2) ) {
-				condition = false
-			}
-		}
-		if (condition == false) {
+	int sizeL1 = numElem(L1)
+	int sizeL2 = numElem(L2)
+	
+	index elementL1 = 0		// index is an unsigned int
+	index elementL2 = 0
+	index elementL3 = 0
+	//elementL1 -> 4
+	//L1 -> 
+	//L2 -> 10, 11, 12, 13, 14, 15
+	while ( elementL1<sizeL1 and elementL2<sizeL2) {
+		if(Recuperar(elementL1, L1) < Recuperar(elementL2, L2) ){
 			Insertar(elementL3, Recuperar(elementL1, L1), L3)
+			++elementL1
 			++elementL3
-			condition = true
 		}
-		++elementL1
-		elementL2 = 0
+		elif (Recuperar(elementL2, L2) < Recuperar(elementL1, L1) ) {
+			//Insertar(elementL3, Recuperar(elementL2, L2), L3)
+			++elementL2
+			++elementL3
+		}
+		else {
+			++elementL1
+			++elementL2
+		}
 	}
+	
+	while (elementL1<sizeL1) {
+		Insertar(elementL3, Recuperar(elementL1, L1), L3)
+		++elementL1
+		++elementL3
+    }
+	/*
+    while (elementL2<sizeL2) {
+        Insertar(elementL3, Recuperar(elementL2, L2), L3)
+		++elementL2
+		++elementL3
+    }	*/
 	
 	return L3
 }
