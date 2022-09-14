8e4e8bc2a8f1613b6f22833d041f21b6efe07897
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2022-09-14 17:26:21 -0600

    Problema 3A resuelto

--- 3a.cpp
+++ 3a.cpp
@@ -4,32 +4,37 @@
  */
 
 // function return a list
-list 3a_mergeUnion(L1, L2) {
+List 3A_Merge_Union(L1, L2) 
+{
+	// Initialize union list (to be filled later)
 	Iniciar(L3)
-	position element = Primera(L1)
-	bool exist = true
 	
-	while ( element != nullPos ) {
+	// Copy L1's elements into L3
+	Position element = Primera(L1)
+	while (element != nullPos)
+	{
 		AgregarAlFinal(Recuperar(element, L1), L3)
 		element = Siguiente(element, L1)
-		// elementL3 = Siguiente(elementL3, L3)
 	}
 	
+	// Find non-duplicates of L3's elements on L2
+	bool exist = true
 	element = Primera(L2)
-	
-	while ( element != nullPos ) {
-		position elementL3 = Primera(L3) 
+	while (element != nullPos) 
+	{
+		Position elementL3 = Primera(L3)
 		// loop to verify if the element already exist in the L3 list
-		while ( elementL3 != nullPos && existe==true) {
-			if ( Recuperar(element, L2) != Recuperar(elementL3, L3) ) {
+		while (elementL3 != nullPos && exist==true) 
+		{
+			if (Recuperar(element, L2) != Recuperar(elementL3, L3))
 				exist = false
-			}
-			elementL3 = Siguiente(elementL3, L3)
+			else
+				elementL3 = Siguiente(elementL3, L3)
 		}
 		
-		if (exist == false) {
+		if (exist == false)
 			AgregarAlFinal(Recuperar(element, L2), L3)	
-		}
+		
 		element = Siguiente(element, L2)
 		exist = true
 	}
