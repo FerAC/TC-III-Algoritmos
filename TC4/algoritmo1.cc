964fa3c8b7880c340fc05d463372cb47d28c41a0
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-19 12:25:27 -0600

    Update algoritmo1.cc

---
+++ TC4/algoritmo1.cc
@@ -0,0 +1,15 @@
+/*
+1. Eliminar todo el subárbol que se genera a partir de un nodo n. Debe hacer un recorrido
+por niveles. Se sugiere que en lugar de usar un modelo Cola, use un modelo Lista
+Indexada. Recuerde que el operador básico de borrado requiere que el nodo sea una hoja.
+*/
+
+void deleteSubTree(node N) {
+		
+	
+	
+	
+
+
+}
+
