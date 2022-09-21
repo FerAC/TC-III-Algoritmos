ece7d4d0373858919e8637095872a104e8720c10
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-21 09:38:04 -0600

    added question 3's algorithm

--- TC4/algoritmo3.cc
+++ TC4/algoritmo3.cc
@@ -2,8 +2,21 @@
 3. Averiguar cuántos nodos hay en el i-ésimo nivel. Debe hacer un recorrido en pre orden.
 */
 
-int cantityNodesInLevel(node N, int level) {
-	
-	
-	
+int cantityNodes = 0
+
+void cantityNodesInLevel(node N, int level) {
+	if(level == 0) {
+		node child = N.HijoMásIzq()
+		while(child != null) {
+			++ cantityNodes
+			child = child.hermanoDer()
+		}
+		return
+	} else {
+		node child = N.HijoMásIzq()
+		while(child != null) {
+			cantityNodesInLevel(child, level-1)
+			child = child.hermanoDer()
+		}
+	}
 }
