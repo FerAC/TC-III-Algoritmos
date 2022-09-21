64aea36023749e37dab40f6b58919edb3443b0df
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-21 09:23:17 -0600

    added question 2 algorithm

--- TC4/algoritmo2.cc
+++ TC4/algoritmo2.cc
@@ -2,8 +2,27 @@
 2. Averiguar cuántas hojas tiene un árbol. Debe hacer un recorrido en pre orden. Se
 sugiere que el contador de hojas sea una variable global.
 */
-int cantityLeaves(node N) {
-	
-	
-	
+
+global int cantityLeaves = 0;	// will be used 
+global Node recursiv
+
+void cantityLeaves(node N) {
+	if(N.HijoMásIzq() == null) {
+		++cantityLeaves
+		return
+	} else {
+		node child = N.HijoMásIzq()
+		while(child != null) {
+			cantityLeaves(child)
+			child = child.hermanoDer()
+		}
+	}
 }
+
+
+
+
+
+// recursive
+//go down to the left until finding leaves, if right brother is no leaves, then go down, else increment counter and go up and down
+// if leaves icnrement, else 
