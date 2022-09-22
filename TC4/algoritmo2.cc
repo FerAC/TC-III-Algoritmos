35df8e8a2411373e8bcb5142456e6d3ccd6b7159
Javier Solano <javier.solanosaltachin@ucr.ac.cr>
2022-09-21 21:53:03 -0600

    Algoritmo 2 terminado

--- TC4/algoritmo2.cc
+++ TC4/algoritmo2.cc
@@ -3,26 +3,23 @@
 Se sugiere que el contador de hojas sea una variable global.
 */
 
-int quantityLeaves = 0	// will be used as a counter to keep track of the cantity of leaves in the tree
+int quantityLeaves = 0	// Will be used as a global counter to keep track of the cantity of leaves in the tree
 
-void quantityLeavesInTree(Arbol tree, Node N) {
-	if(HijoMásIzq(N, tree) == null) {
+void quantityLeavesInTree(Arbol tree, Nodo subroot) {
+	// Base case: The subroot IS the leaf. Add 1 to the leaf counter
+	if(HijoMásIzq(subroot, tree) == null) {
 		++quantityLeaves
+
 		return
-	} else {
-		Node child = HijoMásIzq(N, tree)
+	}
+	// Recursive case: The subroot is NOT a leaf. We must consider the sum of the children's leafs.
+	else {
+		Nodo child = HijoMásIzq(subroot, tree)
 		while(child != null) {
 			quantityLeavesInTree(child)
 			child = hermanoDer(child, tree)
 		}
+
 		return
 	}
 }
-
-
-
-
-
-// recursive
-//go down to the left until finding leaves, if right brother is no leaves, then go down, else increment counter and go up and down
-// if leaves icnrement, else 
