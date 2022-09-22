3c2b831fcedb4ee97038d4e12f21687fc8485bc4
FerAC <87688596+FerAC@users.noreply.github.com>
2022-09-22 00:21:34 -0600

    Implementacion cola (remake)

--- TC4/algoritmo4.cc
+++ TC4/algoritmo4.cc
@@ -3,4 +3,31 @@
    contiene una Lista Indexada L, tal que L contiene las (k^i – 1) / (k-1) etiquetas del árbol. 
  * Las etiquetas están acomodadas en L de acuerdo al orden de un recorrido por niveles de A.
  */
-
+Tree createTree(i , k , L){
+	int counter = 0
+	int numChilds = 0
+	int numElements =  (k^i – 1) / (k-1) 
+	
+	Cola queue
+	Iniciar(queue)
+	Tree myTree
+	Iniciar(myTree)
+	PonerRaiz(Recuperar(counter, L))
+	counter++
+	node nodeTemp = Raiz(myTree)
+	
+	while(counter< numElements){
+		while(numChilds < k){
+			AgregarHijo(nodeTemp, Recuperar(counter, L), myTree )
+			counter++
+			numChilds++
+			Encolar(nodeTemp, queue)
+		}
+		numChilds = 0
+		nodeTemp = Desencolar(queue) 
+	}
+	
+	Destruir(queue)
+	
+	return myTree 
+}
