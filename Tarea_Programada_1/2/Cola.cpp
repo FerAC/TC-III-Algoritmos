4320d8c42c76a0c72289a186f22a957053c40dc4
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-10-08 17:46:19 -0600

    debuging changes

--- Tarea_Programada_1/2/Cola.cpp
+++ Tarea_Programada_1/2/Cola.cpp
@@ -2,35 +2,41 @@
 
 Cola::Cola(int maxSize)
 {
-    size = maxSize;
-    arrayCircular = new int[size];
-    head = 0;
-    end = size-1;	
+	size = maxSize;
+	arrayCircular = new int[size];
+	head = 0;
+	end = size - 1;
 }
 
 Cola::~Cola()
 {
-    delete arrayCircular;
+	delete arrayCircular;
 }
 
 /**
  * @brief El metodo Encolar permite hacer un push a la cola, agrega un element al final de la cola
  */
 void Cola::Encolar(int newElement)
-{ 
-    // check if the queue is not full
-    if(this->NumElem() != size){ 
+{
+	// check if the queue is not full
+	//if (this->NumElem() != size)
+	//{
 
-	if(end==size-1){
-		end =0;
-	}else{
-		++end;
-	}
+	//	if (end == size - 1)
+	//	{
+	//		end = 0;
+	//	}
+	//	else
+	//	{
+	//		++end;
+	//	}
 
-	arrayCircular[end] = newElement;
-    }else{
-	// cannot insert element, queue already full
-    }
+	//	arrayCircular[end] = newElement;
+	//}
+	//else
+	//{
+		// cannot insert element, queue already full
+	//}
 }
 
 /**
@@ -39,29 +45,38 @@ void Cola::Encolar(int newElement)
  */
 int Cola::Desencolar()
 {
-		int bufferPop = arrayCircular[head]; // there is no need to clean the current value of the poped element
-		
-		if(head==size-1){
-			head = 0;
-		}else{
-			++head;
-		}
-		return bufferPop;
+	/*
+	int bufferPop = arrayCircular[head]; // there is no need to clean the current value of the poped element
+
+	if (head == size - 1)
+	{
+		head = 0;
+	}
+	else
+	{
+		++head;
+	}
+	return bufferPop;
+	*/
 }
 
 /*
  * @brief El metodo NumElem devuelve la cantidad de elemento en el array circular
  */
 int Cola::NumElem()
-{	
-	if(head==end){
+{
+	if (head == end)
+	{
 		return 0;
 	}
-	if(end>head){
-		return (end-head); 
-	} else{
-		return (size-(size-head)+end);
+	if (end > head)
+	{
+		return (end - head);
+	}
+	else
+	{
+		return (size - (size - head) + end);
 	}
 	// if end es mayor a head, entonces end-head elementos
-	// else es size-(size-head) + end 
+	// else es size-(size-head) + end
 }
--- Tarea_Programada_1/2/mainCola.cpp
+++ Tarea_Programada_1/2/mainCola.cpp
@@ -2,37 +2,38 @@
 #include <iostream>
 #include "Cola.h"
 
-int main(){
+int main()
+{
 	int colaSize = 10;
-     	Cola cola(colaSize);
+	Cola cola(colaSize);
 
-    	cola.Encolar(0);
-	cola.Encolar(1);
-	cola.Encolar(2);	
-	cola.Encolar(3);
-	cola.Encolar(4);
-	cola.Encolar(5);
-	cola.Encolar(6);
+	cola.Encolar(0);
+	//cola.Encolar(1);
+	//cola.Encolar(2);
+	//cola.Encolar(3);
+	//cola.Encolar(4);
+	//cola.Encolar(5);
+	// cola.Encolar(6);
+	//cola.Encolar(7);
+	//cola.Encolar(7);
+	//cola.Encolar(8);
+
+	//cola.Desencolar();
+	//cola.Desencolar();
 	//cola.Encolar(9);
-	cola.Encolar(7);
-	cola.Encolar(8);
+	// cola.Encolar(10);
+	// must try case where queue is full
 
-	cola.Desencolar();
-	cola.Desencolar();
-	cola.Encolar(9);
-	//cola.Encolar(10);
-	//must try case where queue is full
-	
 	int quantityElement = cola.NumElem();
 	std::cout << "Inicial quantity of elements in the queue: " << cola.NumElem() << std::endl;
 
 	int counter = 0;
-	while(counter<quantityElement){
+	while (counter < quantityElement)
+	{
 		// pop and print element
 		std::cout << cola.Desencolar() << std::endl;
 		++counter;
 	}
 
-
-    	 return 0;
+	return 0;
 }
