5d4959cea65fd78976cf4f8f87a0c792fdc4105d
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-10-08 12:23:47 -0600

    Update Cola.cpp
    finished pop/desencolar function

--- Tarea_Programada_1/2/Cola.cpp
+++ Tarea_Programada_1/2/Cola.cpp
@@ -38,11 +38,15 @@ void Cola::Encolar(int newElement)
  */
 int Cola::Desencolar()
 {
-	if(end!=head){
+	if(end!=head){	//else queue is empty
 		int bufferPop = arrayCircular[head];	// there is no need to "clean" the current value of the poped element
 		
 		//ifelse to change head value
-		
+		if(head==0){
+			head = size-1;
+		}else{
+			--head;
+		}
 		return bufferPop;
 	} else{
 		return 0;
