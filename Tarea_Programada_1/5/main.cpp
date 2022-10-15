3e6fdc9d4d99b11ae5f060d5e3eb7612cf35a517
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-10-15 14:23:50 -0600

    added basic skeleton for question 5 answer

---
+++ Tarea_Programada_1/5/main.cpp
@@ -0,0 +1,58 @@
+//#include "Tree_senaladorPadre.h"          // 3.1
+//#include "ArbolLH.h"                      // 3.2
+//#include "Arbol.cpp"                      // 3.3
+//#include "treePunteroPadreNoContador.h"   // 3.4
+//#include "tree5.h"                        // 3.5
+#include <iostream>
+
+int main()
+{
+
+    int choice;
+    int seguir = 1;
+
+    while (seguir)
+    {
+        std::cout << "\n\n\t############################# \n" << std::endl;
+        std::cout << "Que desea hacer con el arbol n-ario?" << std::endl;
+        std::cout << "0 - cambiar raiz (vital al inicializar el arbol)" << std::endl;
+        std::cout << "1 - agregar hijo" << std::endl;
+        std::cout << "2 - borrar hoja" << std::endl;
+        std::cout << "3 - imprimir el arbol" << std::endl;
+        std::cout << "4 - get raiz" << std::endl;
+        std::cout << "5 - salir y destruir el arbol" << std::endl;
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 0: // cambiar raiz (vital al inicializar el arbol)
+            /* code */
+            break;
+
+        case 1: // agregar hijo
+            /* code */
+            break;
+
+        case 2: // borrar hoja
+            /* code */
+            break;
+
+        case 3: // imprimir el arbol
+            /* code */
+            break;
+
+        case 4: // get raiz
+            /* code */
+            break;
+
+        case 5: // salir y destruir arbol
+            seguir = 0;
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
