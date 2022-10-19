d7bc3766977da848a94083dfc93a6e5f0ee5a513
FerAC <87688596+FerAC@users.noreply.github.com>
2022-10-19 15:08:47 -0600

    NodoConcreto

--- Tarea_Programada_1/3.2/Intento2/NodoConcreto.h
+++ Tarea_Programada_1/3.2/Intento2/NodoConcreto.h
@@ -1,29 +1,29 @@
-#ifndef NODOLH
-#define NODOLH
+#ifndef NodoConcretoLH
+#define NodoConcretoLH
 #include "Lista.h"
 #include "Celda.h"
 #include <iostream>
 using namespace std;
 
 class NodoConcreto{
-	friend ostream& operator<<(ostream& salida, Nodo p){
+	friend ostream& operator<<(ostream& salida, NodoConcreto p){
 			return p.imprimir(salida);  
 		}
 	private:
 		int etiqueta;
-		Lista<Nodo*> * listaHijos; 
+		Lista<NodoConcreto*> * listaHijos; 
 		
 	public:
-		Nodo();
-		Nodo(int); 
-		~Nodo();
-		void insertarEnLista(Nodo*);
-		Lista<Nodo*>* getListaHijos(); 
+		NodoConcretoConcreto();
+		NodoConcreto(int); 
+		~NodoConcreto();
+		void insertarEnLista(NodoConcreto*);
+		Lista<NodoConcreto*>* getListaHijos(); 
 		int getEtiqueta();
 		void setEtiqueta(int);
 		ostream & imprimir(ostream&); 
 		void imprimirValor();
-		int operator==(Nodo); 
+		int operator==(NodoConcreto); 
 		 
 	
 };
