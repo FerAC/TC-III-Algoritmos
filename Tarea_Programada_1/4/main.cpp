4deab6693818f0b0152a834a0581c75bacf21dc3
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-10-19 21:05:23 -0600

    Update main.cpp

--- Tarea_Programada_1/4/main.cpp
+++ Tarea_Programada_1/4/main.cpp
@@ -1,4 +1,5 @@
 //#include "Tree_senaladorPadre.h"          // 3.1
+#include "3.1.hpp"
 //#include "ArbolLH.h"                      // 3.2
 //#include "Arbol.cpp"                      // 3.3
 //#include "treePunteroPadreNoContador.h"   // 3.4
@@ -34,6 +35,9 @@ int main()
 
         if (respuestaUsuario == 'a')
         {
+
+            Arbol arbol;
+
             std::cout << "\n\n\t ############################# \n"
                       << std::endl;
             std::cout << "Que desea hacer con el arbol n-ario?" << std::endl;
@@ -42,17 +46,21 @@ int main()
             std::cout << "2 - borrar hoja" << std::endl;
             std::cout << "3 - imprimir el arbol" << std::endl;
             std::cout << "4 - get raiz" << std::endl;
-            std::cout << "5 - salir y destruir el arbol" << std::endl;
+            std::cout << "5 - imprimir arbol" << std::endl;
+            std::cout << "6 - salir y destruir el arbol" << std::endl;
             std::cin >> choice;
 
             switch (choice)
             {
             case 0: // cambiar raiz (vital al inicializar el arbol)
-                /* code */
+                std::cout << "Cual valor quiere poner a la raiz ?" << std::endl;
+                int valorRaiz;
+                std::cin >> valorRaiz;
+                arbol.PonerRaiz(valorRaiz);
                 break;
 
             case 1: // agregar hijo
-                /* code */
+                
                 break;
 
             case 2: // borrar hoja
@@ -67,7 +75,11 @@ int main()
                 /* code */
                 break;
 
-            case 5: // salir y destruir arbol
+            case 5: // imprimir
+                // imprimir
+                break;
+
+            case 6: // salir y destruir arbol
                 seguir = 0;
                 break;
 
