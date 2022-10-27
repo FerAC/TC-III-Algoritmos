5145b25103f5cfacafec5895942eede61e19ce51
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-10-27 10:31:08 -0600

    Create Ejercicio 4.cpp

---
+++ TC6/Ejercicio 4.cpp
@@ -0,0 +1,7 @@
+// hacer unrecorrido en profundidad primero y numerar a los vertices de un grafo dirigido sin aristas paralelas y 
+// sin lazos de acuerdo al orden de devolucion de la llamada recursiva.
+// para realizar dicha numericacion utilice una Lsita Indexada, tal que L(i) contenga al i-esimo vertice que hace
+// devolucion de llamada recursiva
+
+
+
