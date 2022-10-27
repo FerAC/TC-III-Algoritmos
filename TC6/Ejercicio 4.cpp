eb5694873ecc7ea5b2faee6a908717f9887e73cd
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-10-27 11:14:58 -0600

    finished Ejercicio 4 algorithm

--- TC6/Ejercicio 4.cpp
+++ TC6/Ejercicio 4.cpp
@@ -4,4 +4,33 @@
 // devolucion de llamada recursiva
 
 
+// para poder saber si un vertice ya fue visitado, tenemos que usar una estrucutra de datos addicional, vamos a usar
+// una arreglo indexado, el arreglo es global
 
+// el algortimo devuelve el arregloIndexado donde cada indice del arreglo representa el valor numerico dado al
+// vertice de este mismo indice
+
+arregloIndexado recorridoProfundidadPrimero(Grafo grafo){
+	Arreglo visitados = arreglo.Iniciar(numElem(grafo))
+	
+	RPPrecursivo(grafo, primerVertice(grafo))
+
+	destruir(visitados)
+	
+	return grafo
+}
+
+void RPPrecursivo(Grafo grafo, Vertice vertice){
+
+	visitados.agregarAlFinal(vertice) // se agrega el vertice a la lista de vertice visitados
+	Vertice adyacente = primerVerticeAdyacente(vertice, grafo)
+	
+	// para cada vertice adyacente de vertice
+	mientras ( adyacente != verticeNulo) {
+		// si adyacente no existe en la lista de los vertices visitados, entonces llamar funcion
+		si (!existe(adyacente, visitados)){
+			RPPrecursivo(grafo, adyacente)
+		}
+		adyacente = siguienteVerticeAdyacente(vertice, 	
+	} 
+}
