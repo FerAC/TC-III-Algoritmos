7194fb0afe1b1f506c3ef600788578593fc2a958
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-11-17 10:18:15 -0600

    added main.cpp file
    added main.cpp file and a few basic test instructions

--- Tarea_Programada_2/src/Algoritmos/Controlador.hpp
+++ Tarea_Programada_2/src/Algoritmos/Controlador.hpp
@@ -1,20 +1,44 @@
-#include "../MatrizAdyacencia/Grafo.hpp"
-#include <iostream>
-
 #ifndef CONTROLADOR_ALGORITMOS_GRAFOS
 #define CONTROLADOR_ALGORITMOS_GRAFOS
 
-class Controlador{
-    private:
-        Grafo grafo;
-    public:
-    Controlador(){}
-    ~Controlador(){}
-    void Kruskal(){
-        
-    }
-
-};
+#if defined(MATRIZ)
+#include "../MatrizAdyacencia/Grafo.hpp"
+#define VERSION_GRAFO "GRAFO REPRESENTADO CON MATRIZ DE ADYACENCIA"
+#else
+#include "../ListaAdyacencia/Grafo.hpp"
+#define VERSION_GRAFO "GRAFO REPRESENTADO CON LISTA DE ADYACENCIA"
 #endif
 
+#include <iostream>
 
+class Controlador
+{
+private:
+   // Grafo grafo;
+
+public:
+    Controlador() {}
+
+    ~Controlador() {}
+
+    static void Prim()
+    {
+    }
+
+    static void Kruskal()
+    {
+    }
+
+    static void Dijkstra()
+    {
+    }
+
+    static void Floyd()
+    {
+    }
+
+    static void imprimirGrafo(Grafo grafo){
+        return;
+    }
+};
+#endif
---
+++ Tarea_Programada_2/src/Algoritmos/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * compile:  g++ main.cpp
+*/
+#include "Controlador.hpp"
+
+
+
+int main(){
+
+    Grafo grafo;
+    Controlador::imprimirGrafo(grafo);
+
+
+    return 0;
+
+
+}
